1a3439f6eee24d7742233eaf6734e2f1a0e281d7
liufei_csd <18797948361@163.com>
2020-04-10 09:20:26 +0800

    Initial Commit

---
+++ components/Myheader/myheader.js
@@ -0,0 +1,27 @@
+// components/Myheader/myheader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    cData:{
+      value:"默认组件",
+      type:String
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
